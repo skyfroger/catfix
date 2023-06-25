dcf2e6783cb2305ae668f06a523db03a8dedf0ce
skyfroger <skyfroger@gmail.com>
2023-06-25 23:40:24 +0300

    dev: прототип чтения скриптов из проекта

--- src/components/GradesList.tsx
+++ src/components/GradesList.tsx
@@ -3,16 +3,38 @@
  */
 
 import React, { useState } from "react";
+import {ScratchProject, Target} from "../../@types/scratch";
 import { toScratchblocks } from "parse-sb3-blocks/dist/parse-sb3-blocks.module";
 
 interface gradesListProps {
-    project: JSON;
+    project: ScratchProject | undefined;
 }
 
 function GradesList({ project }: gradesListProps) {
+
+    console.log(project);
+    let whenGreenflag: string[];
+    let scripts: string[] = [];
+    if (project !== undefined) {
+        const stage = project.targets.filter((t: Target) => !t.isStage)[0];
+        console.log(stage);
+        whenGreenflag = Object.keys(stage.blocks).filter(
+            (key) => stage.blocks[key].opcode === "event_whenflagclicked"
+        );
+        whenGreenflag.forEach((cap)=>{
+            scripts.push(toScratchblocks(cap, stage.blocks, "en", {
+                tab: "  ",
+                variableStyle: "always"
+            }))
+        })
+
+    }
+
     return (
         <div>
-            <pre>{JSON.stringify(project)}</pre>
+            {scripts.map((item, index)=>{
+                return (<pre key={index}>{item}</pre>)
+            })}
         </div>
     );
 }
