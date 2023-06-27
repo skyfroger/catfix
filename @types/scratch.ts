b7b36f792b923f57d1b0ba7f9dbd5feeafd5ac32
skyfroger <skyfroger@gmail.com>
2023-06-27 20:47:50 +0300

    ref: типы для json

--- @types/scratch.ts
+++ @types/scratch.ts
@@ -7,80 +7,88 @@ type JSONValue =
     | number
     | boolean
     | undefined
+    | null
     | { [x: string]: JSONValue }
     | Array<JSONValue>;
 
 type Monitor = {
-    id: string,
-    mode: string,
-    opcode: string,
-    params: JSONValue,
-    spriteName?: string,
-    value: number,
-    width: number,
-    height: number,
-    x: number,
-    y: number,
-    visible: boolean,
-    sliderMin: number,
-    sliderMax: number
-}
+    id: string;
+    mode: string;
+    opcode: string;
+    params: JSONValue;
+    spriteName?: string;
+    value: number;
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+    visible: boolean;
+    sliderMin: number;
+    sliderMax: number;
+};
 
 type Block = {
-    opcode: string,
-    next?: string,
-    parent?: string,
-    inputs: JSONValue,
-    fields: JSONValue,
-    shadow: boolean,
-    topLevel: boolean,
+    opcode: string;
+    next?: string;
+    parent?: string;
+    inputs:
+        | {
+        CONDITION: Array<number | null>;
+        SUBSTACK: Array<number | string>;
+    }
+        | JSONValue;
+    fields: JSONValue;
+    shadow: boolean;
+    topLevel: boolean;
     mutation: {
-        tagName: string,
-        children: Array<JSONValue>,
-        proccode: string,
-        argumentids: Array<JSONValue>,
-        argumentnames: Array<JSONValue>,
-        argumentdefaults: Array<JSONValue>,
-        warp: boolean
-    },
-    x?: number,
-    y?: number
-}
+        tagName: string;
+        children: Array<JSONValue>;
+        proccode: string;
+        argumentids: Array<JSONValue>;
+        argumentnames: Array<JSONValue>;
+        argumentdefaults: Array<JSONValue>;
+        warp: boolean;
+    };
+    x?: number;
+    y?: number;
+};
 
 export type Target = {
-    isStage: boolean,
-    name: string,
-    variables: {[key: string]: Array<string|number>},
-    lists: {[key: string]: Array<string|number>},
-    broadcasts: {[key: string]: string},
-    blocks: {[key: string]: Block},
-    comments: {[key: string]: {
-        blockId: string,
-            x: number,
-            y: number,
-            width: number,
-            height: number,
-            minimized: boolean,
-            text: string
-        }},
-    currentCostume: number,
-    costumes: JSONValue,
-    sounds: JSONValue,
-    volume: number,
-    layerOrder: number,
-    tempo: number,
-    videoTransparency?: number,
-    videoState: string,
-    textToSpeechLanguage: string
-}
+    isStage: boolean;
+    name: string;
+    variables: { [key: string]: Array<string | number> };
+    lists: { [key: string]: Array<string | number> };
+    broadcasts: { [key: string]: string };
+    blocks: { [key: string]: Block };
+    comments: {
+        [key: string]: {
+            blockId: string;
+            x: number;
+            y: number;
+            width: number;
+            height: number;
+            minimized: boolean;
+            text: string;
+        };
+    };
+    currentCostume: number;
+    costumes: JSONValue;
+    sounds: JSONValue;
+    volume: number;
+    layerOrder: number;
+    tempo: number;
+    videoTransparency?: number;
+    videoState: string;
+    textToSpeechLanguage: string;
+};
 
 export type ScratchProject = {
-    targets: Target[],
-    monitors: Monitor[],
-    extensions: string[],
+    targets: Target[];
+    monitors: Monitor[];
+    extensions: string[];
     meta: {
-        semver: string,
-        vm: string,
-        agent: string
-    }
-}
+        semver: string;
+        vm: string;
+        agent: string;
+    };
+};
