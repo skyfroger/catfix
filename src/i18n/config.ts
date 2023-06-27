22b657d7ee414939898842140ad21dee4c976997
skyfroger <skyfroger@gmail.com>
2023-06-27 20:50:14 +0300

    ref: форматирование

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,25 +1,23 @@
-import  i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
 import ru from "./locales/ru.json";
 import by from "./locales/by.json";
 
-const resources = {
+export const resources = {
     ru: {
-        main: ru
+        main: ru,
     },
     by: {
-        main: by
-    }
-}
+        main: by,
+    },
+};
 
-i18n
-    .use(initReactI18next)
-    .init({
-        resources,
-        defaultNS: 'main',
-        debug: true,
-        fallbackLng: 'ru',
-    });
+i18n.use(initReactI18next).init({
+    resources,
+    defaultNS: "main",
+    debug: true,
+    fallbackLng: "ru",
+});
 
 export default i18n;
