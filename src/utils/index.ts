863a1743b54bad24e933aca21ae10b5c2f898853
skyfroger <skyfroger@gmail.com>
2023-06-27 20:48:31 +0300

    fix: вылет при парсинге некоторых блоков

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,98 +2,109 @@
  * Вспомогательные функции для парсинга проекта
  */
 
-import {Project, Sprite} from "../../@types/parsedProject";
-import {ScratchProject, Target} from "../../@types/scratch";
+import { Project, Sprite } from "../../@types/parsedProject";
+import { ScratchProject, Target } from "../../@types/scratch";
 
 import { toScratchblocks } from "parse-sb3-blocks/dist/parse-sb3-blocks.module";
 
 // список кодов для блоков-тригеров
 const HAT_BLOCKS = [
-    'event_whenflagclicked',
-    'event_whenkeypressed',
-    'event_whengreaterthan',
-    'event_whenthisspriteclicked',
-    'event_whenstageclicked',
-    'event_whenbackdropswitchesto',
-    'event_whenbroadcastreceived',
-    'control_start_as_clone',
-    'procedures_definition',
-    'boost_whenColor',
-    'boost_whenTilted',
-    'ev3_whenButtonPressed',
-    'ev3_whenDistanceLessThan',
-    'ev3_whenBrightnessLessThan',
-    'gdxfor_whenGesture',
-    'gdxfor_whenForcePushedOrPulled',
-    'gdxfor_whenTilted',
-    'makeymakey_whenMakeyKeyPressed',
-    'makeymakey_whenCodePressed',
-    'microbit_whenButtonPressed',
-    'microbit_whenGesture',
-    'microbit_whenTilted',
-    'microbit_whenPinConnected',
-    'wedo2_whenDistance',
-    'wedo2_whenTilted',
+    "event_whenflagclicked",
+    "event_whenkeypressed",
+    "event_whengreaterthan",
+    "event_whenthisspriteclicked",
+    "event_whenstageclicked",
+    "event_whenbackdropswitchesto",
+    "event_whenbroadcastreceived",
+    "control_start_as_clone",
+    "procedures_definition",
+    "boost_whenColor",
+    "boost_whenTilted",
+    "ev3_whenButtonPressed",
+    "ev3_whenDistanceLessThan",
+    "ev3_whenBrightnessLessThan",
+    "gdxfor_whenGesture",
+    "gdxfor_whenForcePushedOrPulled",
+    "gdxfor_whenTilted",
+    "makeymakey_whenMakeyKeyPressed",
+    "makeymakey_whenCodePressed",
+    "microbit_whenButtonPressed",
+    "microbit_whenGesture",
+    "microbit_whenTilted",
+    "microbit_whenPinConnected",
+    "wedo2_whenDistance",
+    "wedo2_whenTilted",
 ];
 
 // код блока с названием процедуры пользователя
-const PROCEDURES_PROTOTYPE = 'procedures_prototype';
+const PROCEDURES_PROTOTYPE = "procedures_prototype";
 
 function parseTarget(sprite: Target): Sprite {
     /**
      * Парсинг полей отдельного спрайта
      */
 
-    // заготовка спрайта
+        // заготовка спрайта
     let parsedSprite: Sprite = {
-        name: '',
-        scripts: [],
-        customBlocks: [],
-        localVars: [],
-        localLists: []
-    }
+            name: "",
+            scripts: [],
+            customBlocks: [],
+            localVars: [],
+            localLists: [],
+        };
 
     // сохраняем имя спрайта
     parsedSprite.name = sprite.name;
 
     // сохраняем имена локальных переменных
-    for (const key in sprite.variables){
-        parsedSprite.localVars.push(String(sprite.variables[key][0]))
+    for (const key in sprite.variables) {
+        parsedSprite.localVars.push(String(sprite.variables[key][0]));
     }
 
     // сохраняем имена локальных списков
-    for (const key in sprite.lists){
-        parsedSprite.localLists.push(String(sprite.lists[key][0]))
+    for (const key in sprite.lists) {
+        parsedSprite.localLists.push(String(sprite.lists[key][0]));
     }
 
     // список id стартовых блоков спрайта
-    const spriteCapIDs = Object.keys(sprite.blocks).filter(
-        (key) => HAT_BLOCKS.includes(sprite.blocks[key].opcode)
+    const spriteCapIDs = Object.keys(sprite.blocks).filter((key) =>
+        HAT_BLOCKS.includes(sprite.blocks[key].opcode)
     );
 
     // получаем и сохраняем список имён блоков, описанных пользователем
     const customBlockIDs: string[] = Object.keys(sprite.blocks).filter(
         (key) => sprite.blocks[key].opcode === PROCEDURES_PROTOTYPE
     );
-    customBlockIDs.forEach((id:string) => {
+    customBlockIDs.forEach((id: string) => {
         parsedSprite.customBlocks.push(sprite.blocks[id].mutation.proccode);
-    })
+    });
 
     // сохраняем валидные скрипты спрайта (единичный блок-шапка не будет валидным скриптом)
-    spriteCapIDs.forEach((hat)=> {
-
+    spriteCapIDs.forEach((hat) => {
         // получаем удобочитаемый текст скрипта
-        const script: string = toScratchblocks(hat, sprite.blocks, "en", {
-            tab: "  ",
-            variableStyle: "always"
-        });
+        try {
+            const script: string = toScratchblocks(hat, sprite.blocks, "en", {
+                tab: "  ",
+                variableStyle: "always",
+            });
 
-        // если в результате получилась только одна строка, то в скрипте только один блок
-        // такой скрипт не считается валидным
-        if (script.includes("\n")) {
-            parsedSprite.scripts.push(script);
+            // если в результате получилась только одна строка, то в скрипте только один блок
+            // такой скрипт не считается валидным
+            if (script.includes("\n")) {
+                parsedSprite.scripts.push(script);
+            }
+        } catch (e) {
+            /*
+            TODO в библиотеке парсинга есть ошибка: в некоторых случаях в JSON могут
+            быть поля равные null. Парсер перестаёт работать. Пока просто пропускаем
+            такие скрипты
+            */
+            console.error(
+                hat,
+                "В скрипте есть пустое условие или пусткой C-блок. Невозможно обработать!"
+            );
         }
-    })
+    });
 
     return parsedSprite;
 }
@@ -103,22 +114,24 @@ function parseProject(scratchProject: ScratchProject): Project {
      * Парсинг полей проекта
      */
 
-    // заготовка итогового проекта
+        // заготовка итогового проекта
     let project: Project = {
-        broadcasts: [],
-        stage: undefined,
-        sprites: []
-    };
+            broadcasts: [],
+            stage: undefined,
+            sprites: [],
+        };
 
     // парсим сцену проекта
     const stage = scratchProject.targets.filter((t: Target) => t.isStage)[0];
     project.stage = parseTarget(stage);
 
     // парсим спрайты проекта
-    const targets: Target[] = scratchProject.targets.filter((t: Target) => !t.isStage);
+    const targets: Target[] = scratchProject.targets.filter(
+        (t: Target) => !t.isStage
+    );
     targets.forEach((t: Target) => {
         project.sprites.push(parseTarget(t));
-    })
+    });
 
     // получаем список передаваемых сообщений
     project.broadcasts = Object.values(stage.broadcasts);
