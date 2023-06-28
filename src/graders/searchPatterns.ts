d313b73adc5997171bed930a99727267bce7dd34
skyfroger <skyfroger@gmail.com>
2023-06-28 15:38:31 +0300

    feat: regex для поиска условных операторов и стоставных условий

--- src/graders/searchPatterns.ts
+++ src/graders/searchPatterns.ts
@@ -6,3 +6,14 @@ export const countLoopRE = new RegExp("repeat \\((.+)\\)\\n(.+\\n)+end");
 
 // поиск цикла с предусловием
 export const untilLoopRE = new RegExp("repeat until \\<(.+)\\>\\n(.+\\n)+end");
+
+// поиск блока если ... то
+export const ifThenRE = new RegExp("if \\<(.+)\\> then\\n(.+\\n)+end");
+
+// поиска блока если ... то ... иначе
+export const ifThenElseRE = new RegExp(
+    "if \\<(.+)\\> then\\n(.+\\n)+else\\n(.+\\n)+end"
+);
+
+// поиск составных условий с and, or и not
+export const compConditionsRE = new RegExp("<(.)+> (and|or) <(.)+>|<not (.)+>");
