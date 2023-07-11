1944c41633bcf678209d6f218cad83a5e0d42856
skyfroger <skyfroger@gmail.com>
2023-07-11 16:26:36 +0300

    ref: удалены неиспользуемые файлы

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./i18n/config";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
