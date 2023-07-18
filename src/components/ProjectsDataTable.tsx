f557bf6b37ca99b439a2d00b262d89da64bc8d84
skyfroger <skyfroger@gmail.com>
2023-07-18 15:23:27 +0300

    feat: парсим, оцениваем и получаем список советов для каждого загруженного проекта

--- src/components/ProjectsDataTable.tsx
+++ src/components/ProjectsDataTable.tsx
@@ -1,7 +1,22 @@
 import react from "react";
 import { Card, Col, message, Row, Space } from "antd";
+import React from "react";
+import { categories, graderResult } from "../graders";
+import { Tip } from "../scaners/types";
+export interface TableData {
+    key: React.Key;
+    projectName: string;
+    projectAuthor: string;
+    totalGrade: number;
+    grades: Map<categories, graderResult>;
+    tips: Tip[];
+}
 
-function ProjectsDataTable() {
+interface propsDataTable {
+    data: TableData[];
+}
+function ProjectsDataTable({ data }: propsDataTable) {
+    console.log(data);
     return (
         <>
             <div>Таблица с результатами</div>
--- src/components/TeacherPage.tsx
+++ src/components/TeacherPage.tsx
@@ -1,10 +1,16 @@
-import react, { useState } from "react";
+import react, { useEffect, useState } from "react";
 import { Card } from "antd";
 import MassURLLoader from "./MassURLLoader";
-import ProjectsDataTable from "./ProjectsDataTable";
+import ProjectsDataTable, { TableData } from "./ProjectsDataTable";
 import { APIResponce } from "../utils/httpAPI";
+import parseProject from "../utils";
+import grader, { getTotalGrade } from "../graders";
+import { Tip } from "../scaners/types";
+import { scanForErrors, scanForWarnings } from "../scaners";
+
 function TeacherPage() {
     const [projectsData, setProjectsData] = useState<APIResponce[]>([]);
+    const [tableData, setTableData] = useState<TableData[]>([]);
 
     /**
      * Сохранение в state массива проектов
@@ -15,13 +21,50 @@ function TeacherPage() {
         setProjectsData(projects);
     };
 
+    useEffect(() => {
+        // как только получены новые проекты, нужно их парсить, оценить
+        // и получить список советов
+
+        const tableData: TableData[] = [];
+
+        // перебираем загруженные проекты
+        projectsData.forEach((project, index) => {
+            // парсим проект
+            const parsedProject = parseProject(project.projectJSON);
+            // оцениваем проект
+            const grades = grader(parsedProject);
+
+            // суммарная оценка
+            const totalGrade = getTotalGrade(grades);
+
+            // получаем массив советов (ошибок и предупреждений)
+            const tips: Tip[] = [
+                ...scanForErrors(parsedProject, project.projectJSON),
+                ...scanForWarnings(parsedProject, project.projectJSON),
+            ];
+
+            // сохраняем данные для строки в таблице
+            tableData.push({
+                key: index,
+                projectName: project.projectName,
+                projectAuthor: project.projectAuthor,
+                grades: grades,
+                tips: tips,
+                totalGrade: totalGrade,
+            });
+        });
+
+        // меняем state
+        setTableData(tableData);
+    }, [projectsData]);
+
     return (
         <>
             <Card style={{ marginBottom: 16 }}>
                 <MassURLLoader onUpload={handleURLUpload} />
             </Card>
             <Card style={{ marginBottom: 16 }}>
-                <ProjectsDataTable />
+                <ProjectsDataTable data={tableData} />
             </Card>
         </>
     );
