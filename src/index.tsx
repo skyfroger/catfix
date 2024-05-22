7c2089ec30fd5f57b43f43d23acfe638ebba4a36
skyfroger <skyfroger@gmail.com>
2024-05-22 19:42:17 +0300

    feat: подготовка к деплою в подкаталог

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <Router>
+        <Router basename={process.env.REACT_APP_BASE_URL || "/"}>
             <App />
         </Router>
     </React.StrictMode>
