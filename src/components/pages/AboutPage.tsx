c178564054b785b8d7e355ca255de35a5996dcc0
skyfroger <skyfroger@gmail.com>
2024-12-26 01:09:27 +0300

    feat: md контент

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -5,8 +5,22 @@ import { useTranslation } from "react-i18next";
 import { Divider, Card, Typography } from "antd";
 import { Helmet } from "react-helmet-async";
 
+import ReactMarkdown from "react-markdown";
+import { useEffect, useState } from "react";
+
 function AboutPage() {
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
+    const [content, setContent] = useState<string | null>(null);
+
+    useEffect(() => {
+        import(`../../content/about.${i18n.language}.md`).then((res) => {
+            fetch(res.default)
+                .then((res) => res.text())
+                .then((res) => setContent(res))
+                .catch((err) => console.log(err));
+        });
+    });
+
     return (
         <>
             <Helmet>
@@ -25,7 +39,7 @@ function AboutPage() {
                 <Card>
                     <Typography.Title>{t("ui.menuAbout")}</Typography.Title>
                     <Divider />
-                    {parse(t("ui.aboutDescription"))}
+                    <ReactMarkdown children={content} />
                 </Card>
             </motion.div>
         </>
---
+++ src/content/about.en.md
@@ -0,0 +1,5 @@
+The application helps evaluate Scratch projects and find errors in scripts.
+
+Developer: Pavel Aleksandrovich Khoroshevich, Senior Lecturer of the Department of Informatics and Methodology of Informatics Teaching at the Faculty of Physics and Mathematics of BSPU named after Maxim Tank.
+
+You can read more about the project in [my blog](https://cs-labs.netlify.app/blog/posts/2023/08/kotfix/). The source code of the project is available on the [GitHub page](https://github.com/skyfroger/catfix).
