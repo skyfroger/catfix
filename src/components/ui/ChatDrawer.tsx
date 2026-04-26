1678244cc98931bb089f18a8a92dab736374e351
skyfroger <skyfroger@gmail.com>
2026-04-26 22:51:22 +0300

    feat: чат в боковом окне

---
+++ src/components/ui/ChatDrawer.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { Drawer, FloatButton } from "antd";
+import { MessageOutlined } from "@ant-design/icons";
+import ChatHOC from "../chat/ChatHOC";
+
+function ChatDrawer() {
+    const [open, setOpen] = useState(false);
+
+    const showDrawer = () => {
+        setOpen(true);
+    };
+
+    const onClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <Drawer
+                title="Ментор"
+                closable={{ "aria-label": "Close Button" }}
+                onClose={onClose}
+                open={open}
+                size={"min(100vw, 45vw)"}
+                mask={{ blur: true }}
+                style={{
+                    backgroundColor: "hsl(10, 30%, 98%)",
+                    borderLeft: "1px solid #121b33",
+                }}
+                styles={{
+                    body: {
+                        padding: "0 0 0 1.2em",
+                    },
+                }}
+            >
+                <ChatHOC />
+            </Drawer>
+
+            <FloatButton
+                icon={<MessageOutlined />}
+                type="primary"
+                onClick={showDrawer}
+                style={{ right: 24, bottom: 24 }}
+            />
+        </>
+    );
+}
+
+export default ChatDrawer;
